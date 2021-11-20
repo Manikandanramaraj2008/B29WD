a4aa9574819a856c7e2b9df27cce30c21e5b9e40
maniramkr2006@gmail.com <maniramkr2008@gmail.com>
2021-11-20 23:23:12 +0530

    link and script added

--- heckathon github assignment/script.js
+++ heckathon github assignment/script.js
@@ -1,19 +1,20 @@
 getAllData("Raj");
 var title =document.createElement("h1");
 title.setAttribute("id","myHeading");
-title.innerText: ('GITHUB API OF USERS AND REPOS');
+title.innerText=("GITHUB API OF USERS AND REPOS");
 document.body.appendChild(title);
 
-var indexCard =document.createElement("div");
-indexCard.setAttribute("id","inputDiv");
+var indCard =document.createElement("div");
+indCard.setAttribute("id","inputDiv");
 
 async function getAllData(username)
 {
     try
     {
-        var data = await fetch("https://api.github.com/users"+username);
+        var data = await fetch("https://api.github.com/users/"+username);
         var obj =  await data.json();
-        console.log(obj);
+        console.log(obj.id);
+        console.log(obj.repos_url);
     }
 
     catch(error)
