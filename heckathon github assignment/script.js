78ebd7d02ae6e6c07d6f8b1b2515e367269fedcf
maniramkr2006@gmail.com <maniramkr2008@gmail.com>
2021-11-20 14:08:17 +0530

    changes in API

--- heckathon github assignment/script.js
+++ heckathon github assignment/script.js
@@ -1,7 +1,7 @@
 getAllData("Raj");
 var title =document.createElement("h1");
 title.setAttribute("id","myHeading");
-title.innerText('GITHUB API OF USERS AND REPOS');
+title.innerText: ('GITHUB API OF USERS AND REPOS');
 document.body.appendChild(title);
 
 var indexCard =document.createElement("div");
@@ -11,8 +11,8 @@ async function getAllData(username)
 {
     try
     {
-        var data = fetch("https://api.github.com/users"+username);
-        var obj =  await.data.json();
+        var data = await fetch("https://api.github.com/users"+username);
+        var obj =  await data.json();
         console.log(obj);
     }
 
