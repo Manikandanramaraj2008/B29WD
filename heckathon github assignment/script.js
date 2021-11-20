1c2666c9dea4130589aeccd6fadf7cc4e34e09f5
maniramkr2006@gmail.com <maniramkr2008@gmail.com>
2021-11-20 13:48:10 +0530

    codeforGithubAPI

---
+++ heckathon github assignment/script.js
@@ -0,0 +1,31 @@
+getAllData("Raj");
+var title =document.createElement("h1");
+title.setAttribute("id","myHeading");
+title.innerText('GITHUB API OF USERS AND REPOS');
+document.body.appendChild(title);
+
+var indexCard =document.createElement("div");
+indexCard.setAttribute("id","inputDiv");
+
+async function getAllData(username)
+{
+    try
+    {
+        var data = fetch("https://api.github.com/users"+username);
+        var obj =  await.data.json();
+        console.log(obj);
+    }
+
+    catch(error)
+    {
+        console.log('error in data fetching')
+    }
+}
+console.log('check1')
+
+
+
+
+
+
+
